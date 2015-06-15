680e28345ed5e166899a1193636c64bc5856b393
Karven Lam <karven.lam@nyu.edu>
2015-06-14 23:29:24 -0400

    small update

--- app/src/main/java/com/ravenlamb/android/texteditor/MainActivity.java
+++ app/src/main/java/com/ravenlamb/android/texteditor/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,9 +16,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends ListActivity {
@@ -64,8 +68,34 @@ public class MainActivity extends ListActivity {
             }else if(item.equalsIgnoreCase("Favorites")){
 
             }else if(item.equalsIgnoreCase("Browse Directories")){
-
+                currentList= new ArrayList<String>();
+                isInitialList=false;
                 //todo root list and external storage
+                File[] rootlist= File.listRoots();
+                for(int i=0;i<rootlist.length;i++){
+                    currentList.add(rootlist[i].getAbsolutePath());
+                }
+                if(isExternalStorageReadable()){
+
+//            Environment.DIRECTORY_ALARMS, Environment.DIRECTORY_DCIM, Environment.DIRECTORY_DOCUMENTS
+//            Environment.DIRECTORY_DOWNLOADS, Environment.DIRECTORY_MOVIES,
+//            Environment.DIRECTORY_MUSIC, Environment.DIRECTORY_PICTURES, Environment.DIRECTORY_PODCASTS
+//            Environment.DIRECTORY_RINGTONES
+                    //Toast.makeText(this, "external storage is readable", Toast.LENGTH_SHORT).show();
+                    if(Build.VERSION.SDK_INT >=19){
+                        currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath());
+                        //externalPublic.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS));
+                    }
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_RINGTONES).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS).getAbsolutePath());
+                    currentList.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_ALARMS).getAbsolutePath());
+
+                }
             }else if(item.equalsIgnoreCase("Recent Directories")){
 
             }else if(item.equalsIgnoreCase("Recent Files")){
@@ -73,7 +103,8 @@ public class MainActivity extends ListActivity {
             }
         }else
         {
-
+            String currentFilePath=item;
+            historyList.add(currentFilePath);
         }
     }
 
@@ -99,6 +130,27 @@ public class MainActivity extends ListActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /* Checks if external storage is available for read and write */
+    //copied from android.com
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    //copied from android.com
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
     class NavigationListAdapter extends ArrayAdapter<String> {
         public NavigationListAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
             super(context, resource, textViewResourceId, objects);
@@ -110,12 +162,12 @@ public class MainActivity extends ListActivity {
                     (Context.LAYOUT_INFLATER_SERVICE);
             View row= inflater.inflate(R.layout.list_item,null,false);
             TextView textview= (TextView) row.findViewById(R.id.itemView);
-            String currentFileName=MainActivity.this.currentList.get(position);
+            String currentFilePath=MainActivity.this.currentList.get(position);
             if(isInitialList) {
-                textview.setText(currentFileName);
+                textview.setText(currentFilePath);
                 textview.setTextColor(Color.BLACK);
             }else{
-                File currentFile= new File(currentFileName);
+                File currentFile= new File(currentFilePath);
                 textview.setText(currentFile.getName());
                 textview.setTextColor(Color.BLACK);
                 if(!currentFile.canWrite()){
