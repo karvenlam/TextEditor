a2b5b78f930b93deebbb4e897e9285bd020f623c
Karven Lam <karven.lam@nyu.edu>
2015-09-08 00:17:14 -0400

    currently broken, trying to add InputConnection and custom EditText

--- app/src/main/java/com/ravenlamb/android/texteditor/LineEditText.java
+++ app/src/main/java/com/ravenlamb/android/texteditor/LineEditText.java
@@ -1,6 +1,7 @@
 package com.ravenlamb.android.texteditor;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.widget.EditText;
 
@@ -27,5 +28,42 @@ public class LineEditText extends EditText {
         //// TODO: 8/31/2015 don't know what to do yet
     }
 
+//http://stackoverflow.com/questions/4886858/android-edittext-deletebackspace-key-event
+//    @Override
+//    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+//        return new ZanyInputConnection(super.onCreateInputConnection(outAttrs),
+//                true);
+//    }
+//
+//    private class ZanyInputConnection extends InputConnectionWrapper {
+//
+//        public ZanyInputConnection(InputConnection target, boolean mutable) {
+//            super(target, mutable);
+//        }
+//
+//        @Override
+//        public boolean sendKeyEvent(KeyEvent event) {
+//            if (event.getAction() == KeyEvent.ACTION_DOWN
+//                    && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+//                ZanyEditText.this.setRandomBackgroundColor();
+//                // Un-comment if you wish to cancel the backspace:
+//                // return false;
+//            }
+//            return super.sendKeyEvent(event);
+//        }
+//
+//    }
 
+
+
+
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        // Get the text to print
+////        final float textSize = super.getTextSize();
+//        final String text = super.getText().toString();
+//
+////        canvas.drawText(text, getPaddingStart(), floaty, getPaint());
+//        super.onDraw(canvas);
+//    }
 }
